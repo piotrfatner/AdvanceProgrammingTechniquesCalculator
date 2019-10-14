4a2a2eba07d7bba38cd76252edda0f1d3c84b928
piotrfatner <fatner.piotr@gmail.com>
2019-10-15 00:13:49 +0200

    Cpp & CS Calculators

--- JavaCalculator/src/javacalculator/JavaCalculator.java
+++ JavaCalculator/src/javacalculator/JavaCalculator.java
@@ -16,6 +16,7 @@ public class JavaCalculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello, Calculator!");
     }
     
 }
