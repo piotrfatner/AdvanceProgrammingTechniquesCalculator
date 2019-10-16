0a889f63925054ee021a5254ce4c2fff7b75b0c1
WooCash93 <lukaszma93@gmail.com>
2019-10-16 18:22:38 +0200

    commit 3

--- JavaCalculator/src/javacalculator/JavaCalculator.java
+++ JavaCalculator/src/javacalculator/JavaCalculator.java
@@ -1,5 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package javacalculator;
+
 import java.util.Scanner; //morrison
+
 /**
  *
  * @author fatne, morrison
